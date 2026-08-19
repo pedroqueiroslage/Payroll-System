fc20fe8527917b863cb6d0bf81b6d043bb3c3f70
Pedro <pedroqueiroslage09@gmail.com>
2026-08-19 14:04:06 -0300

    methods

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class Funcionario(ABC):
-    def __init__(self, nome=None, sal_bruto=0, sal_min=1621, inss=7.5):
+    def __init__(self, nome=None, sal_bruto=0, sal_min=1621, inss=0):
         self.nome = nome
         self.sal_bruto = sal_bruto
         self.sal_min = sal_min
@@ -13,4 +13,13 @@ class Funcionario(ABC):
         pass
 
     def analisar_sal(self):
+        pass
+
+    def cad_funcionario(self):
+        pass
+
+    def relatorio(self):
+        pass
+
+    def res_empresa(self):
         pass
