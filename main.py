962dc694204099f985e1c30d4fdf98944b8737d0
Pedro <pedroqueiroslage09@gmail.com>
2026-08-19 15:11:17 -0300

    t2

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from rich import print
 
 
 class Funcionario(ABC):
-    def __init__(self, nome=None, sal_bruto=0, sal_min=1621, inss=0.0):
+    def __init__(self, nome=None, sal_min=1621):
         self.nome = nome
-        self.sal_bruto = sal_bruto
         self.sal_min = sal_min
-        if sal_bruto <= 1412:
-            self.inss = 0.075
-        elif 1412.01 < sal_bruto < 2666.68:
-            self.inss = 0.09
-        elif 2666.69 < sal_bruto < 4000.03:
-            self.inss = 0.12
-        elif 4000.03 < sal_bruto:
-            self.inss = 0.14
+        self.sal_bruto = 0.0
+        self.desc_inss = 0.0
+        self.sal_liq = 0.0
+
+    def calc_inss(self):
+        if self.sal_bruto <= 1412:
+            aliquota = 0.075
+        elif 1412.01 <= self.sal_bruto <= 2666.68:
+            aliquota = 0.09
+        elif 2666.69 <= self.sal_bruto <= 4000.03:
+            aliquota = 0.12
         else:
-            self.inss = inss
+            aliquota = 0.14
+        self.desc_inss = self.sal_bruto * aliquota
+        return self.desc_inss
 
     @abstractmethod
     def calc_sal(self):
@@ -44,9 +48,9 @@ class Horista(Funcionario):
 
     def calc_sal(self):
         self.sal_bruto = self.val_hora * self.hrs_trab
-        self.inss = self.sal_bruto * self.inss
-        salf = self.sal_bruto - self.inss
-        print(salf)
+        self.calc_inss()
+        self.sal_liq = self.sal_bruto - self.desc_inss
+        print(self.sal_liq)
 
 
 class Mensalista(Funcionario):
@@ -55,10 +59,10 @@ class Mensalista(Funcionario):
 
 
 def main():
-    # p = str(input('Qual tipo de funcionario?\nH = Horista\nM = Mensalista\n'))
-    # if p == 'H':
-    #     Horista('Pedro', 50, 30)
-    Horista('Pedro', 50, 30)
+    p = str(input('Qual tipo de funcionario?\nH = Horista\nM = Mensalista\n'))
+    if p == 'H':
+        p1 = Horista('Pedro', 50, 100)
+        p1.calc_sal()
 
 
 if __name__ == '__main__':
